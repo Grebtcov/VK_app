a729cd1f599bf4a4b8e54ef14e79d31944b50cd8
Alex <Ryllezz55@gmail.com>
2022-03-03 21:47:13 +0300

    Initial Commit

---
+++ VK_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_app
+//
+//  Created by Alex x on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
